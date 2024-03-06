0c30bf7351eaf3321faf0e470c50fc0d76352fae
Jose Alberto Hernandez <alberto@black-box.lan>
2024-03-06 07:49:06 -0600

    Undo Account Transfer

--- src/app/account-transfers/view-account-transfer/view-account-transfer.component.ts
+++ src/app/account-transfers/view-account-transfer/view-account-transfer.component.ts
@@ -2,6 +2,10 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AccountTransfersService } from '../account-transfers.service';
+import { TranslateService } from '@ngx-translate/core';
+import { ConfirmationDialogComponent } from 'app/shared/confirmation-dialog/confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'mifosx-view-account-transfer',
@@ -17,7 +21,10 @@ export class ViewAccountTransferComponent {
    * @param {Location} location Location.
    */
   constructor(private route: ActivatedRoute,
-    private location: Location) {
+    private location: Location,
+    private accountTransfersService: AccountTransfersService,
+    public dialog: MatDialog,
+    private translateService: TranslateService) {
     this.route.data.subscribe((data: { viewAccountTransferData: any }) => {
       this.viewAccountTransferData = data.viewAccountTransferData;
     });
@@ -33,6 +40,22 @@ export class ViewAccountTransferComponent {
 
   goBack(): void {
     this.location.back();
+  }
+
+  undoTransaction(): void {
+    const undoTransactionAccountDialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: { heading: this.translateService.instant('labels.heading.Undo Transaction'), dialogContext: 
+        this.translateService.instant('labels.dialogContext.Are you sure you want undo the transaction') + `${this.viewAccountTransferData.id}` }
+    });
+    undoTransactionAccountDialogRef.afterClosed().subscribe((response: { confirm: any }) => {
+      if (response.confirm) {
+        this.accountTransfersService.adjustAccountTransfer(this.viewAccountTransferData.id, {}).subscribe((response: any) => {
+          console.log(response);
+          this.goBack();
+        });
+      }
+    });
+
   }
 
 }
